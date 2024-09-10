1d0b0c69c8652a49de57938165ebdfd2e98c0a13
Luis Miguel Martin Luengo <luismiguelm@gmail.com>
2024-09-10 14:02:28 +0200

    Primeras pruebas con variables

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,10 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more 
+
+
+
 Console.WriteLine("Hola, World!");
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("Azul???");
 Console.ForegroundColor = ConsoleColor.White;
 Console.Write("Congratulations!");
 Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
-String a = "\nHoli";
-Console.WriteLine(a);
+Console.Write("You wrote your first lines of code.\n");
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.WriteLine("the second line.");
+Console.WriteLine("123");
+Console.WriteLine(123);
+Console.WriteLine("true");
+Console.WriteLine(true);
+string ejemplo_string = "hola";
+char ejemplo_char = 'a';
+int ejemplo_int = 7;
+
