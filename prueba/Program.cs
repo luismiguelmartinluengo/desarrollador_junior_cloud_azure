04447a60fd239d3ef0a84f0fa1f4fd76c541e967
Luis Miguel Martin Luengo <luismiguelm@gmail.com>
2024-09-11 14:04:07 +0200

    Ejercicio interpolacion de texto y literales reales

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -48,3 +48,11 @@ Console.WriteLine(message);
 Console.WriteLine("\n-------------------Probando interpolacion junto con string literal----------------------------");
 string projectName = "First-Project";
 Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string englishMessage = "View English output:";
+string russianPath = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+string englishPath = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"{englishMessage}\n\t{englishPath}");
+Console.WriteLine($"{russianMessage}\n\t{russianPath}");
