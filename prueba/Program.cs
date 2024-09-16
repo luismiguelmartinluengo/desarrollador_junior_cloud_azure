2c103fe66efc86bee4bce1197e141da144058a79
Luis Miguel Martin Luengo <luismiguelm@gmail.com>
2024-09-16 11:12:00 +0200

    Ejercicio condicional 2 mejorado

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,21 +1,20 @@
 Random random = new Random();
 
-//int discountPercentage = 0;
+bool continua = true;
 
-while (true){
+while (continua){
         int daysUntilExpiration = random.Next(12);
         if (daysUntilExpiration < 11){
             if (daysUntilExpiration > 4){
                 Console.WriteLine("Your subscription will expire soon. Renew now!!");
-                Console.WriteLine(daysUntilExpiration);
-
             }else if (daysUntilExpiration >1){
-                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\tRenew now and save 10%!");
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%!");
             }else if (daysUntilExpiration >0){
-                Console.WriteLine($"Your subscription expires within a day!\tRenew now and save 20%!");
+                Console.WriteLine($"Your subscription expires within a day!\nRenew now and save 20%!");
+            }else{
                 Console.WriteLine("Your subscription has expired.");
             }//End if
-        };//End 
+        };//End if
 
         // Esperar a que el usuario pulse una tecla
         Console.WriteLine("Presiona cualquier tecla para volver a ejecutar o 'Esc' para salir.");
@@ -23,6 +22,6 @@ while (true){
 
         // Salir del bucle si se pulsa la tecla 'Esc'
         if (key == ConsoleKey.Escape){
-            break;
+            continua = false;
         }//End f
 }//End if
