c3f1900a9c8b590cf9082f4516ce3bd0e295ebe2
Luis Miguel Martin Luengo <luismiguelm@gmail.com>
2024-09-16 10:47:03 +0200

    Ejercicio condicional 2

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,34 +1,28 @@
-Random dados = new();
+Random random = new Random();
 
-for(int i = 0; i < 5; i++){
-    Console.WriteLine(dados.Next(1,7));
-}// End for
+//int discountPercentage = 0;
 
-int firstValue = 500;
-int secondValue = 600;
-Console.WriteLine(Math.Max(firstValue, secondValue));
+while (true){
+        int daysUntilExpiration = random.Next(12);
+        if (daysUntilExpiration < 11){
+            if (daysUntilExpiration > 4){
+                Console.WriteLine("Your subscription will expire soon. Renew now!!");
+                Console.WriteLine(daysUntilExpiration);
 
-Random dice = new Random();
+            }else if (daysUntilExpiration >1){
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\tRenew now and save 10%!");
+            }else if (daysUntilExpiration >0){
+                Console.WriteLine($"Your subscription expires within a day!\tRenew now and save 20%!");
+                Console.WriteLine("Your subscription has expired.");
+            }//End if
+        };//End 
 
-int[] rolls = new int[3];
-for(int i = 0; i < 3; i++){
-    rolls[i] = dice.Next(1,7);
-}//End for
+        // Esperar a que el usuario pulse una tecla
+        Console.WriteLine("Presiona cualquier tecla para volver a ejecutar o 'Esc' para salir.");
+        var key = Console.ReadKey(true).Key;
 
-int bonus = 0;
-if ((rolls[0] == rolls[1]) && (rolls[1] == rolls[2])){
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    bonus = 6;
-} else if ((rolls[0] == rolls[1]) || (rolls[1] == rolls[2]) || (rolls[0] == rolls[2])){
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    bonus = 2;
-}//End if
-
-Console.WriteLine($"Dice roll bucle: {rolls[0]} + {rolls[1]} + {rolls[2]} + bonus = {rolls.Sum()+bonus}");
-
-
-if (rolls.Sum() + bonus > 10){
-    Console.WriteLine("You win!");
-}else{
-    Console.WriteLine("Sorry, you lose.");
+        // Salir del bucle si se pulsa la tecla 'Esc'
+        if (key == ConsoleKey.Escape){
+            break;
+        }//End f
 }//End if
