a146fbe3d458f9e283f57082da184a705c37704c
Luis Miguel Martin Luengo <luismiguelm@gmail.com>
2024-09-16 11:55:02 +0200

    Matrices y foreach

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,27 +1,9 @@
-Random random = new Random();
+Random random = new();
 
-bool continua = true;
-
-while (continua){
-        int daysUntilExpiration = random.Next(12);
-        if (daysUntilExpiration < 11){
-            if (daysUntilExpiration > 4){
-                Console.WriteLine("Your subscription will expire soon. Renew now!!");
-            }else if (daysUntilExpiration >1){
-                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%!");
-            }else if (daysUntilExpiration >0){
-                Console.WriteLine($"Your subscription expires within a day!\nRenew now and save 20%!");
-            }else{
-                Console.WriteLine("Your subscription has expired.");
-            }//End if
-        };//End if
-
-        // Esperar a que el usuario pulse una tecla
-        Console.WriteLine("Presiona cualquier tecla para volver a ejecutar o 'Esc' para salir.");
-        var key = Console.ReadKey(true).Key;
-
-        // Salir del bucle si se pulsa la tecla 'Esc'
-        if (key == ConsoleKey.Escape){
-            continua = false;
-        }//End f
-}//End if
+int[] matriz = new int[6];
+for (int i = 0; i < 6; i++){
+    matriz[i] = random.Next(100);
+}//End for
+foreach(int i in matriz){
+    Console.WriteLine(i);
+}//End for
