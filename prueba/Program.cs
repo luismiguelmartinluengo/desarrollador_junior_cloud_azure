b40bf2513f2ad0780a1765fecf454ce37e5ba892
Luis Miguel Martin Luengo <luismiguelm@gmail.com>
2024-09-18 14:04:55 +0200

    Info de variables

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,19 +1,17 @@
-Random random = new();
-int herohealth = 20;
-int monsterhealth = 20;
-bool isHeroTurn = true;
-int damage;
+Console.WriteLine("Signed integral types:");
 
-do {
-    damage = random.Next(1,11);
-    if (isHeroTurn){
-        monsterhealth -= damage;
-        Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterhealth} health.");
-    }else{
-        herohealth -= damage;
-        Console.WriteLine($"Hero was damaged and lost {damage} health and now has {herohealth} health.");
-    }//End if
-    isHeroTurn = !isHeroTurn;
-    Console.ReadKey();
-}while(herohealth>0 && monsterhealth>0);
-Console.WriteLine($"{((herohealth <= 0)?"Monster":"Hero")} wins!!!");
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+Console.WriteLine("\nUnsigned integral types:");
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+Console.WriteLine("\nFloating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
