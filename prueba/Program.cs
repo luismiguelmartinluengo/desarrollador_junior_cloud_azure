077b8e75b56c12c5a59f20b5584057fa359c4649
Luis Miguel Martin Luengo <luismiguelm@gmail.com>
2024-09-20 10:59:16 +0200

    Basicos de formato

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,8 +1,25 @@
-string pangram = "The quick brown fox jumps over the lazy dog";
+using System.Globalization;
 
-string[] words = pangram.Split(" ");
-for(int i = 0; i<words.Length; i++){
-    words[i] = string.Join("", words[i].ToCharArray().Reverse());
-}//End for
-Console.WriteLine(pangram);
-Console.WriteLine(string.Join(" ",words));
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N4} units");
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P}");
+
+decimal price2 = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = string.Format("You saved {0:C2} off the regular {1:C2} price. ", price2 - salePrice, price);
+Console.WriteLine(yourDiscount);
+
+string input = "Pad this";
+ Console.WriteLine(input.PadLeft(12, '-'));
+ Console.WriteLine(input.PadLeft(12, '-'));
+ Console.WriteLine(input.PadRight(12, '-'));
