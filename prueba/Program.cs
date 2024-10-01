90ca12f87dbfdba3a378f06586a47deff8629673
Luis Miguel Martin Luengo <luismiguelm@gmail.com>
2024-10-01 11:19:17 +0200

    Ejercicio con metodos

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,9 +1,41 @@
-Console.WriteLine(DateTime.Now.ToLongTimeString());
-Console.WriteLine(DateTime.UtcNow.ToLongTimeString());
-Console.WriteLine(DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.ffff"));
+using System.Runtime.CompilerServices;
 
-DateTime hoy = DateTime.UtcNow;
-Console.WriteLine(hoy.ToUniversalTime());
-Console.WriteLine(hoy.AddMonths(1));
-DateTime fecha = new DateTime(2023,1,30);
-Console.WriteLine(fecha.AddMonths(1).Ticks);
+string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+
+string externalDomain = "hayworth.com";
+string interanlDomain = "contoso.com";
+
+
+string getUserName(string _firstName, string _lastName) {
+    return _firstName[..2].ToLower() + _lastName.ToLower();
+}//End getUserName
+
+string getEmail(int _index, bool _isExternal = false){
+    if (_isExternal) {
+        string userName = getUserName(external[_index,0], external[_index,1]);
+        return userName + "@" + externalDomain;
+    }else{
+        string userName = getUserName(corporate[_index,0], corporate[_index,1]);
+        return userName + "@" + interanlDomain;
+    }//End if
+}//End getEmail
+
+for (int i = 0; i < corporate.GetLength(0); i++) {
+    Console.WriteLine(getEmail(i));
+}//End for
+
+for (int i = 0; i < external.GetLength(0); i++) {
+    Console.WriteLine(getEmail(i,true));
+}//End for
